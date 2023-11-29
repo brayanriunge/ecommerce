47f16a72f35bc82a7c9dbbcfe33688a23f17f8c3
RiugeBrian <riungebrayandev@gmail.com>
2023-11-29 09:01:45 +0300

    Navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  return (
+    <div className="flex z-30 bg-gray-50">
+      {/* on the left */}
+      <div></div>
+      {/* on the right */}
+      <div></div>
+    </div>
+  );
+}
