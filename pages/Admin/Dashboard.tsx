6d8ebd4ac0520932a36e57eb4b17f76640ffac05
RiugeBrian <riungebrayandev@gmail.com>
2023-11-30 11:59:56 +0300

    dashboard

--- pages/Admin/Dashboard.tsx
+++ pages/Admin/Dashboard.tsx
@@ -1,3 +1,20 @@
+import { useForm } from "react-hooks-useform";
+
+type FormValues = {
+  name: string;
+  description: string;
+  quantity: number;
+};
+
 export default function Dashboard() {
-  return <div>hey</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>();
+  return (
+    <div>
+      <form onSubmit={handleSubmit()}></form>
+    </div>
+  );
 }
