5454c67ff66b896335e6babc4d2af83542a95ce7
RiugeBrian <riungebrayandev@gmail.com>
2023-12-01 15:22:07 +0300

    designing form for adding products

--- pages/Admin/Dashboard.tsx
+++ pages/Admin/Dashboard.tsx
@@ -59,11 +59,11 @@ export default function Dashboard() {
                   type="image"
                   placeholder="picture"
                   required
-                  className="rounded-xl px-6 py-5 focus:outline-none w-5/6 border-dotted"
+                  className="rounded-xl px-10 py-5 focus:outline-none w-5/6 border-dotted"
                 />
               </div>
               <button
-                className="rounded-xl px-6 py-4 bg-orange-400 mt-10 "
+                className="rounded-xl px-6 py-2 bg-orange-400 mt-10 "
                 type="submit"
               >
                 <p className="font-bold hover:text-white">Submit</p>
