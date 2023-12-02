da7c17b7b570d9233de24dcf22b65523e1422016
RiugeBrian <riungebrayandev@gmail.com>
2023-12-02 15:49:59 +0300

    admin register product backend

---
+++ pages/api/admin/register.ts
@@ -0,0 +1,8 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    if (req.method === "POST") {
+    }
+  } catch (error) {}
+}
