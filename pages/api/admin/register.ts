73270793eded536417034a3a75ca13c141ea57a5
RiugeBrian <riungebrayandev@gmail.com>
2023-12-06 21:23:45 +0300

    utility function to parse form data using Formidable

--- pages/api/admin/register.ts
+++ pages/api/admin/register.ts
@@ -6,6 +6,22 @@ const getData = async (
   req: NextApiRequest
 ): Promise<{ fields: formidable.Fields; files: formidable.Files }> => {
   const options: formidable.Options = {};
+  options.maxFieldsSize = 4000 * 1024 * 1024; // Set file maximum to 4Gb
+  const form = formidable(options);
+
+  return new Promise<{ fields: formidable.Fields; files: formidable.Files }>(
+    (resolve, reject) => {
+      form.parse(req, (err, fields, files) => {
+        if (err) {
+          reject(err);
+        } else {
+          // Access the fields under the "product" key and parse as JSON
+          const productFields = JSON.parse(fields.product[0]);
+          resolve({ fields: productFields, files });
+        }
+      });
+    }
+  );
 };
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
