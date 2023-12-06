721f4c6a7cf359d1c6f7a2ab9ac451e8a5e654ef
RiugeBrian <riungebrayandev@gmail.com>
2023-12-06 21:03:16 +0300

    adding formidable

--- pages/api/admin/register.ts
+++ pages/api/admin/register.ts
@@ -1,4 +1,12 @@
+import formidable from "formidable";
 import { NextApiRequest, NextApiResponse } from "next";
+
+//Utility function to parse form dat using Formidable
+const getData = async (
+  req: NextApiRequest
+): Promise<{ fields: formidable.Fields; files: formidable.Files }> => {
+  const options: formidable.Options = {};
+};
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
